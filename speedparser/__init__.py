6e7a89a4371e0b4eb68bd34ab024c7ccfec289ab
Zhuravlev Uriy aka stalkerg <stalkerg@gmail.com>
2014-07-29 16:02:42 +0400

    Fix hook work.

--- speedparser/__init__.py
+++ speedparser/__init__.py
@@ -1,3 +1,7 @@
 from .speedparser import parse
+from .speedparser import SpeedParser
+from .speedparser import SpeedParserEntriesRss20
+from .speedparser import SpeedParserFeedRss20
+
 VERSION = (0,1,9)
-__all__ = ['parse', 'VERSION']
+__all__ = ['parse', 'SpeedParser', 'SpeedParserEntriesRss20', 'SpeedParserFeedRss20', 'VERSION']
